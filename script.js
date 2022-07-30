46e1c0ab2877723bd437530ac40af87e9646d133
Nickunj Chopra <nickunjchopra@gmail.com>
2022-07-30 19:00:34 +0530

    Added UI delete and clearFields.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Book Class: Represents a Book
-
 class Book {
   constructor(title, author, isbn) {
     this.title = title;
@@ -9,7 +8,6 @@ class Book {
 }
 
 // UI Class: Handle UI Tasks
-
 class UI { // static class; never instantiated
   static displayBooks() {
     // Sample
@@ -40,31 +38,44 @@ class UI { // static class; never instantiated
       <td>${book.author}</td>
       <td>${book.isbn}</td>
       <td><a class="btn btn-danger btn-sm delete">
-        <i class="fas fa-times"></i>
+        X
       </a></td>
     `;
 
     list.appendChild(row);
+  }
+
+  static deleteBook(eventTarget) {
+    eventTarget.parentElement.parentElement.remove(); // this feels hacky
+  }
+
+  static clearFields() {
+    document.querySelectorAll('#book-form input[type="text"]').forEach((field) => {
+      field.value = '';
+    });
   }
 }
 
 // Store Class: Handles Storage
 
 // Event: Display Books
-
 document.addEventListener('DOMContentLoad', UI.displayBooks());
 
 // Event: Add Book
-
 document.querySelector('#book-form').addEventListener('submit', (e) => {
   e.preventDefault();
 
   const title = document.querySelector('#title').value;
   const author = document.querySelector('#author').value;
   const isbn = document.querySelector('#isbn').value;
-  const book = new Book(title, author, isbn);
 
-  UI.addBookToList(book);
+  UI.addBookToList(new Book(title, author, isbn));
+  UI.clearFields();
 });
 
 // Event: Delete Book
+document.querySelector('#book-list').addEventListener('click', (e) => {
+  if(e.target.classList.contains('delete')) {
+    UI.deleteBook(e.target);
+  }
+});
